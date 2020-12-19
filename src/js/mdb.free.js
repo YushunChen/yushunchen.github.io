64b313c3b43df029dab209f6d4cc295225091922
Oliver Chen <56765896+YushunChen@users.noreply.github.com>
2020-12-19 15:52:30 -0600

    Version Change
    Big changes

---
+++ src/js/mdb.free.js
@@ -0,0 +1,34 @@
+// BOOTSTRAP CORE COMPONENTS
+import Collapse from './bootstrap/src/collapse';
+import Alert from './free/alert';
+import Button from './bootstrap/src/button';
+import Carousel from './free/carousel';
+import Modal from './free/modal';
+import Popover from './free/popover';
+import ScrollSpy from './free/scrollspy';
+import Tab from './free/tab';
+import Tooltip from './free/tooltip';
+import Toast from './free/toast';
+
+// MDB FREE COMPONENTS
+import Input from './free/input';
+import Dropdown from './free/dropdown';
+import Ripple from './free/ripple';
+import Range from './free/range';
+
+export {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Input,
+  Modal,
+  Popover,
+  Ripple,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip,
+  Range,
+};
